c396574330369e11c14c12d2f148b3ea9bb187af
JulianKohlhaas <J.Kohlhaas@gmx.de>
2013-11-04 00:08:29 +0100

    Attribute + Getter und Setter der Klasse "Bestellung" angelegt

--- shop/src/main/java/shop/Bestellverwaltung/domain/Bestellung.java
+++ shop/src/main/java/shop/Bestellverwaltung/domain/Bestellung.java
@@ -1,5 +1,70 @@
 package shop.Bestellverwaltung.domain;
 
+import java.util.Date;
+
+import shop.Kundenverwaltung.domain.Kunde;
+
+/**
+ * @author Julian Kohlhaas
+ * 
+ */
 public class Bestellung {
+	
+	private Long id ;
+	private shop.Kundenverwaltung.domain.Kunde kunde ;
+	private Date bestelldatum ;
+	private double gesamtpreis ;
+	private boolean versendet ;
+	
+	public Bestellung(Long id, Kunde kunde, Date bestelldatum,
+			double gesamtpreis, boolean versendet) {
+		super();
+		this.id = id;
+		this.kunde = kunde;
+		this.bestelldatum = bestelldatum;
+		this.gesamtpreis = gesamtpreis;
+		this.versendet = versendet;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public shop.Kundenverwaltung.domain.Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(shop.Kundenverwaltung.domain.Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	public Date getBestelldatum() {
+		return bestelldatum;
+	}
+
+	public void setBestelldatum(Date bestelldatum) {
+		this.bestelldatum = bestelldatum;
+	}
+
+	public double getGesamtpreis() {
+		return gesamtpreis;
+	}
+
+	public void setGesamtpreis(double gesamtpreis) {
+		this.gesamtpreis = gesamtpreis;
+	}
+
+	public boolean isVersendet() {
+		return versendet;
+	}
+
+	public void setVersendet(boolean versendet) {
+		this.versendet = versendet;
+	}
+	
 
 }
