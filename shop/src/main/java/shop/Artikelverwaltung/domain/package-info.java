8b84c566df1376c1b1e96f28e747492c95d0e434
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2013-11-03 12:10:52 +0100

    package-info.java bei Artikelverwaltung angelegt

---
+++ shop/src/main/java/shop/Artikelverwaltung/domain/package-info.java
@@ -0,0 +1,11 @@
+@XmlAccessorType(FIELD)
+
+package shop.Artikelverwaltung.domain;
+
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+
+/**
+ * @author <a href="mailto:hebj1011@HS-Karlsruhe.de">Bjoern Hetzel</a>
+ */
