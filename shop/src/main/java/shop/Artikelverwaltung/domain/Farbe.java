0ea3768d96a7862ae98f2617c7eb442c861909fe
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2013-11-03 14:05:01 +0100

    Farbe.java angelegt

---
+++ shop/src/main/java/shop/Artikelverwaltung/domain/Farbe.java
@@ -0,0 +1,17 @@
+package shop.Artikelverwaltung.domain;
+
+
+/**
+ * @author <a href="mailto:hebj1011@HS-Karlsruhe.de">Bjoern Hetzel</a>
+ */
+
+
+public enum Farbe {
+	
+	SCHWARZ,
+	ROT,
+	SILBER,
+	WEISS,
+	BLAU;
+
+}
