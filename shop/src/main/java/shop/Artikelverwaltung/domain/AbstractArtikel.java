1213578ce9ce288a6e93ecc45c8136da408d04ec
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2013-11-05 11:16:34 +0100

    nochmal

--- shop/src/main/java/shop/Artikelverwaltung/domain/AbstractArtikel.java
+++ shop/src/main/java/shop/Artikelverwaltung/domain/AbstractArtikel.java
@@ -2,17 +2,13 @@ package shop.Artikelverwaltung.domain;
 
 import java.io.Serializable;
 import java.net.URI;
-import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.bind.annotation.XmlTransient;
 
 import org.codehaus.jackson.annotate.JsonSubTypes;
 import org.codehaus.jackson.annotate.JsonSubTypes.Type;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
-
-import shop.Bestellverwaltung.domain.Bestellung;
 
 /**
  * @author <a href="mailto:hebj1011@HS-Karlsruhe.de">Bjoern Hetzel</a>
