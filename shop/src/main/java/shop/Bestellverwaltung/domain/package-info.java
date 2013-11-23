12d76c628bf33e517544cbb2e8a4fc714fe694fe
JulianKohlhaas <J.Kohlhaas@gmx.de>
2013-11-23 13:27:12 +0100

    Checkstyle behoben die 2te

--- shop/src/main/java/shop/Bestellverwaltung/domain/package-info.java
+++ shop/src/main/java/shop/Bestellverwaltung/domain/package-info.java
@@ -5,7 +5,7 @@ package shop.Bestellverwaltung.domain;
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 
 import javax.xml.bind.annotation.XmlAccessorType;
+
 /**
  * @author <a href="mailto:koju1020@HS-Karlsruhe.de">Julian Kohlhaas</a>  
- *
  */
