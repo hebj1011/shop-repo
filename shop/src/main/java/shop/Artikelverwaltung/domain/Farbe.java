ff85eab0381bb4160d6c021bed7000a8296689be
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2013-11-25 12:55:36 +0100

    farbe geändert

--- shop/src/main/java/shop/Artikelverwaltung/domain/Farbe.java
+++ shop/src/main/java/shop/Artikelverwaltung/domain/Farbe.java
@@ -8,10 +8,40 @@ package shop.Artikelverwaltung.domain;
 
 public enum Farbe {
 	
-	SCHWARZ,
-	ROT,
-	SILBER,
-	WEISS,
-	BLAU;
+	SCHWARZ("S"),
+	ROT("R"),
+	WEISS("W"),
+	BLAU("B"),
+	GELB("G");
 
+	private String internal;
+	
+	private Farbe(String internal) {
+		this.internal = internal;
+	}
+	
+	public String getInternal() {
+		return internal;
+	}
+	
+	public static Farbe build(String internal) {
+		if (internal == null) {
+			return null;
+		}
+		
+		switch (internal) {
+			case "S":
+				return SCHWARZ;
+			case "R":
+				return ROT;
+			case "W":
+				return WEISS;
+			case "B":
+				return BLAU;
+			case "G":
+				return GELB;
+			default:
+				throw new IllegalArgumentException(internal + " ist kein gueltiger Wert fuer Farbe");
+		}
+	}
 }
