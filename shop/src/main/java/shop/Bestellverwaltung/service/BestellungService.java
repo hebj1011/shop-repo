93ad65913a557f7a5f2312054103357030742a76
JulianKohlhaas <J.Kohlhaas@gmx.de>
2013-12-05 20:04:54 +0100

    Interface BestellungService erstellt

---
+++ shop/src/main/java/shop/Bestellverwaltung/service/BestellungService.java
@@ -0,0 +1,19 @@
+package shop.Bestellverwaltung.service;
+
+import java.util.List;
+import java.util.Locale;
+
+import shop.Bestellverwaltung.domain.Bestellung;
+import shop.Kundenverwaltung.domain.Kunde;
+
+/**
+ * @author <a href="mailto:koju1020@HS-Karlsruhe.de">Julian Kohlhaas</a>  
+ */
+
+public interface BestellungService {
+	
+	Bestellung findBestellungById(Long id);
+	List<Bestellung> findBestellungenByKunde(Kunde kunde);
+	Bestellung createBestellung(Bestellung bestellung, Kunde kunde, Locale locale);
+
+}
