9f2ec6c5d87dcd138948bebf6cd33c6db539097f
JulianKohlhaas <J.Kohlhaas@gmx.de>
2013-12-07 18:34:02 +0100

    Annotation AbsenderName erstellt

---
+++ shop/src/main/java/shop/util/mail/AbsenderName.java
@@ -0,0 +1,22 @@
+package shop.util.mail;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * @author <a href="mailto:koju1020@HS-Karlsruhe.de">Julian Kohlhaas</a>  
+ */
+@Qualifier
+@Target({ FIELD, PARAMETER })
+@Retention(RUNTIME)
+@Documented
+public @interface AbsenderName {
+
+}
