e239aafa81d5b539451387506bf1c4e623d5a496
JulianKohlhaas <J.Kohlhaas@gmx.de>
2013-12-07 18:59:01 +0100

    Klasse BestellungServiceMitGeschenkverpackung (Decorator und Delegator) erstellt

---
+++ shop/src/main/java/shop/Bestellverwaltung/service/BestellungServiceMitGeschenkverpackung.java
@@ -0,0 +1,48 @@
+package shop.Bestellverwaltung.service;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+import java.util.Locale;
+
+import javax.decorator.Delegate;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+
+import org.jboss.logging.Logger;
+
+import shop.Bestellverwaltung.domain.Bestellung;
+import shop.Kundenverwaltung.domain.Kunde;
+
+/**
+ * @author <a href="mailto:koju1020@HS-Karlsruhe.de">Julian Kohlhaas</a>  
+ */
+
+public abstract class BestellungServiceMitGeschenkverpackung implements
+		BestellungService {
+
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
+	
+	@Inject
+	@Delegate
+	@Any
+	private BestellungService bs;
+	
+	@Override
+	public Bestellung findBestellungById(Long id) {	
+		return bs.findBestellungById(id);
+	}
+
+	@Override
+	public List<Bestellung> findBestellungenByKunde(Kunde kunde) {
+		return bs.findBestellungenByKunde(kunde);
+	}
+
+	@Override
+	public Bestellung createBestellung(Bestellung bestellung, Kunde kunde,
+			Locale locale) {
+		LOGGER.warn("Geschenkverpackung leider noch nicht vorrätig...sorryyyy");
+		
+		return bs.createBestellung(bestellung, kunde, locale);
+	}
+
+}
