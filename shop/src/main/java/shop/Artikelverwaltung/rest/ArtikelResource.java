7cbab68477f123c0c3f1d0593a967954bc5982f2
Simon Holzmayer <hosi1015 (at) hs-karlsruhe.de>
2013-12-09 21:00:50 +0100

    Checkstyle

--- shop/src/main/java/shop/Artikelverwaltung/rest/ArtikelResource.java
+++ shop/src/main/java/shop/Artikelverwaltung/rest/ArtikelResource.java
@@ -1,6 +1,5 @@
 package shop.Artikelverwaltung.rest;
 
-
 import static shop.util.Constants.ADD_LINK;
 import static shop.util.Constants.LIST_LINK;
 import static shop.util.Constants.REMOVE_LINK;
@@ -48,103 +47,101 @@ import shop.util.rest.UriHelper;
  * @author <a href="mailto:hebj1011@HS-Karlsruhe.de">Bjoern Hetzel</a>
  */
 
-
 @Path("/artikel")
-@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.75" })
+@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75",
+		TEXT_XML + ";qs=0.75" })
 @Consumes
 @RequestScoped
 @Log
 public class ArtikelResource {
-	
-	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
-	
+
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles
+			.lookup().lookupClass());
+
 	@Context
 	private UriInfo uriInfo;
-	
+
 	@Inject
 	private ArtikelService as;
-	
+
 	@Inject
 	private UriHelper uriHelper;
-	
+
 	@PostConstruct
 	private void postConstruct() {
 		LOGGER.debugf("CDI-faehiges Bean %s wurde erzeugt", this);
 	}
-	
+
 	@PreDestroy
 	private void preDestroy() {
 		LOGGER.debugf("CDI-faehiges Bean %s wird geloescht", this);
 	}
-	
+
 	@GET
 	@Produces({ TEXT_PLAIN, APPLICATION_JSON })
 	@Path("version")
 	public String getVersion() {
 		return "1.0";
 	}
-	
+
 	@GET
 	@Path("{id:[1-9][0-9]*}")
 	public Response findArtikelById(@PathParam("id") Long id) {
 		final AbstractArtikel artikel = as.findArtikelById(id);
 		return Response.ok(artikel)
-                       .links(getTransitionalLinks(artikel, uriInfo))
-                       .build();
+				.links(getTransitionalLinks(artikel, uriInfo)).build();
 	}
-	
+
 	private Link[] getTransitionalLinks(AbstractArtikel artikel, UriInfo uriInfo) {
 		final Link self = Link.fromUri(getUriArtikel(artikel, uriInfo))
-	                          .rel(SELF_LINK)
-	                          .build();
+				.rel(SELF_LINK).build();
 
-		final Link list = Link.fromUri(uriHelper.getUri(ArtikelResource.class, uriInfo))
-		                      .rel(LIST_LINK)
-		                      .build();
+		final Link list = Link
+				.fromUri(uriHelper.getUri(ArtikelResource.class, uriInfo))
+				.rel(LIST_LINK).build();
 
-		final Link add = Link.fromUri(uriHelper.getUri(ArtikelResource.class, uriInfo))
-		                     .rel(ADD_LINK)
-		                     .build();
-		
-		final Link update = Link.fromUri(uriHelper.getUri(ArtikelResource.class, uriInfo))
-			                    .rel(UPDATE_LINK)
-			                    .build();
-		
-		final Link remove = Link.fromUri(uriHelper.getUri(ArtikelResource.class, "deleteArtikel", artikel.getId(), uriInfo))
-		                        .rel(REMOVE_LINK)
-		                        .build();
-		
-		return new Link[] {self, list, add, update, remove };
+		final Link add = Link
+				.fromUri(uriHelper.getUri(ArtikelResource.class, uriInfo))
+				.rel(ADD_LINK).build();
+
+		final Link update = Link
+				.fromUri(uriHelper.getUri(ArtikelResource.class, uriInfo))
+				.rel(UPDATE_LINK).build();
+
+		final Link remove = Link
+				.fromUri(
+						uriHelper.getUri(ArtikelResource.class,
+								"deleteArtikel", artikel.getId(), uriInfo))
+				.rel(REMOVE_LINK).build();
+
+		return new Link[] { self, list, add, update, remove };
 	}
-	
+
 	public URI getUriArtikel(AbstractArtikel artikel, UriInfo uriInfo) {
-		return uriHelper.getUri(ArtikelResource.class, "findArtikelById", artikel.getId(), uriInfo);
+		return uriHelper.getUri(ArtikelResource.class, "findArtikelById",
+				artikel.getId(), uriInfo);
 	}
-	
 
 	@POST
-	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
+	@Consumes({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
 	public Response createArtikel(@Valid AbstractArtikel artikel) {
 		artikel = as.createArtikel(artikel);
-		return Response.created(getUriArtikel(artikel, uriInfo))
-			           .build();
+		return Response.created(getUriArtikel(artikel, uriInfo)).build();
 	}
-	
-	
+
 	@PUT
-	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
+	@Consumes({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
 	public void updateArtikel(@Valid AbstractArtikel artikel) {
 		as.updateArtikel(artikel);
 	}
-	
+
 	@DELETE
 	@Path("{id:[1-9][0-9]*}")
 	@Produces
 	public void deleteArtikel(@PathParam("id") Long id) {
 		as.deleteArtikel(id);
 	}
-	
-}
 
+}
