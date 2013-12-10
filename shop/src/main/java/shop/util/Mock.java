bd8e2251341eda9007087a13d2a116f2646b7ca7
Denis Langer <lade1011(at)hs-karlsruhe.de>
2013-12-10 10:09:00 +0100

    Straße und Hausnummer bei erstellung eingefügt

--- shop/src/main/java/shop/util/Mock.java
+++ shop/src/main/java/shop/util/Mock.java
@@ -54,6 +54,8 @@ public final class Mock {
 		adresse.setId(id + 1);        // andere ID fuer die Adresse
 		adresse.setPlz("12345");
 		adresse.setOrt("Testort");
+		adresse.setStrasse("Testweg");
+		adresse.setHausnummer("12");
 		adresse.setKunde(kunde);
 		kunde.setAdresse(adresse);
 		
