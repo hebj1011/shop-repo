58ad99b1a51cea27397772386a7266a7c9a29ba1
Simon Holzmayer <hosi1015 (at) hs-karlsruhe.de>
2014-01-12 13:25:07 +0100

    Checkstyle, META-INF/orm.xml hibernate.console.properties Inhalte geändert

--- shop/src/main/java/shop/util/Mock.java
+++ shop/src/main/java/shop/util/Mock.java
@@ -2,8 +2,6 @@ package shop.util;
 
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
 //import java.util.HashSet;
 import java.util.List;
 //import java.util.Set;
