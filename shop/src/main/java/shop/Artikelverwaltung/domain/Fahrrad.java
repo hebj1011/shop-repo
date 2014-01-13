4b32a92b36a8b45c752928a315c0529cc86c42df
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2014-01-13 15:29:09 +0100

    fahrrad bearbeitet

--- shop/src/main/java/shop/Artikelverwaltung/domain/Fahrrad.java
+++ shop/src/main/java/shop/Artikelverwaltung/domain/Fahrrad.java
@@ -1,13 +1,18 @@
 package shop.Artikelverwaltung.domain;
 
 import static shop.Artikelverwaltung.domain.AbstractArtikel.FAHRRAD;
+import static javax.persistence.FetchType.EAGER;
 
 import javax.persistence.Cacheable;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.persistence.Convert;
 
 import java.util.Set;
 
@@ -23,19 +28,16 @@ public class Fahrrad extends AbstractArtikel {
 
 	private static final long serialVersionUID = 7087800972131210358L;
 	
-	@Column(length = 10)
-	//@Convert(converter = FarbeConverter.class)
-	private Set<Farbe> farbe;
-	
-	public Set<Farbe> getFarbe() {
-		return farbe;
-	}
-	public void setFarbe(Set<Farbe> farbe) {
-		this.farbe = farbe;
-	}
+	@ElementCollection(fetch = EAGER)
+	@CollectionTable(name = "artikel_farbe",
+	                 joinColumns = @JoinColumn(name = "artikel_fk", nullable = false),
+	                 uniqueConstraints =  @UniqueConstraint(columnNames = { "artikel_fk", "farbe" }),
+	                 indexes = @Index(columnList = "artikel_fk"))
+	@Column(table = "artikel_farbe", name = "farbe", length = 1, nullable = false)
+	private Set<Farbe> farben;
 	
 	@Override
 	public String toString() {
-		return "Fahrrad [" + super.toString() + ", Farbe:" + farbe + "]";
+		return "Fahrrad [" + super.toString() + ", Farbe:" + farben + "]";
 	}
 }
