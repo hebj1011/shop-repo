feec1a007feb084efd28f51ae3102ad9b5c30614
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2014-01-13 15:04:18 +0100

    artikel service und mock angepasst

--- shop/src/main/java/shop/Artikelverwaltung/service/ArtikelService.java
+++ shop/src/main/java/shop/Artikelverwaltung/service/ArtikelService.java
@@ -1,15 +1,10 @@
 package shop.Artikelverwaltung.service;
 
 import java.io.Serializable;
-import java.lang.invoke.MethodHandles;
-//import java.math.BigDecimal;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
-import shop.util.Mock;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -19,10 +14,9 @@ import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-import org.jboss.logging.Logger;
-
-//import com.google.common.base.Strings;
+import com.google.common.base.Strings;
 
 import shop.Artikelverwaltung.domain.AbstractArtikel;
 import shop.util.interceptor.Log;
@@ -30,50 +24,42 @@ import shop.util.interceptor.Log;
 /**
  * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
  */
-
 @Dependent
 @Log
 public class ArtikelService implements Serializable {
 	private static final long serialVersionUID = 3076865030092242363L;
-	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
 	@Inject
 	private transient EntityManager em;
 	
-	@PostConstruct
-	private void postConstruct() {
-		LOGGER.debugf("CDI-faehiges Bean %s wurde erzeugt", this);
+	/**
+	 * Suche nach verfuegbaren Artikeln.
+	 * @return Liste der verfuegbaren Artikel.
+	 */
+	public List<AbstractArtikel> findVerfuegbareArtikel() {
+		return em.createNamedQuery(AbstractArtikel.FIND_VERFUEGBARE_ARTIKEL, AbstractArtikel.class)
+				 .getResultList();
+	}
+
+	
+	/**
+	 * Suche den Artikel zu gegebener ID.
+	 * @param id ID des gesuchten Artikels.
+	 * @return Der gefundene Artikel, null sonst.
+	 * @exception ConstraintViolationException zu @NotNull, falls kein Artikel gefunden wurde
+	 */
+	@NotNull(message = "{artikel.notFound.id}")
+	public AbstractArtikel findArtikelById(Long id) {
+		return em.find(AbstractArtikel.class, id);
 	}
 	
-	@PreDestroy
-	private void preDestroy() {
-		LOGGER.debugf("CDI-faehiges Bean %s wird geloescht", this);
-	}
-	
-//	/**
-//	 * Suche nach verfuegbaren Artikeln.
-//	 * @return Liste der verfuegbaren Artikel.
-//	 */
-//	public List<AbstractArtikel> findVerfuegbareArtikel() {
-//		return em.createNamedQuery(AbstractArtikel.FIND_VERFUEGBARE_ARTIKEL, AbstractArtikel.class)
-//				 .getResultList();
-//	}
-//
-//	
-//	/**
-//	 * Suche den Artikel zu gegebener ID.
-//	 * @param id ID des gesuchten Artikels.
-//	 * @return Der gefundene Artikel, null sonst.
-//	 */
-//	public AbstractArtikel findArtikelById(Long id) {
-//		return em.find(AbstractArtikel.class, id);
-//	}
-//	
 	/**
 	 * Suche die Artikel zu gegebenen IDs. 
 	 * @param ids Liste der IDs
 	 * @return Liste der gefundenen Artikel
+	 * @exception ConstraintViolationException zu @Size, falls die Liste leer ist
 	 */
+	@Size(min = 1, message = "{artikel.notFound.ids}")
 	public List<AbstractArtikel> findArtikelByIds(List<Long> ids) {
 		if (ids == null || ids.isEmpty()) {
 			return Collections.emptyList();
@@ -112,77 +98,225 @@ public class ArtikelService implements Serializable {
 		return em.createQuery(criteriaQuery)
 		         .getResultList();
 	}
-//
-//	
-//	/**
-//	 * Suche die Artikel mit gleicher Bezeichnung
-//	 * @param bezeichnung Gemeinsame Bezeichnung der gesuchten Artikel
-//	 * @return Liste der gefundenen Artikel
-//	 */
-//	public List<AbstractArtikel> findArtikelByName(String name) {
-//		if (Strings.isNullOrEmpty(name)) {
-//			return findVerfuegbareArtikel();
-//		}
-//		
-//		return em.createNamedQuery(AbstractArtikel.FIND_ARTIKEL_BY_NAME, AbstractArtikel.class)
-//				 .setParameter(AbstractArtikel.PARAM_NAME, "%" + name + "%")
-//				 .getResultList();
-//	}
-//	
-//	/**
-//	 * Suche Artikel, die preiswerter als ein bestimmter Preis sind
-//	 * @param preis Maximaler Preis
-//	 * @return Liste der Artikel mit einem geringeren Preis als die angegebene Obergrenze
-//	 */
-//	public List<AbstractArtikel> findArtikelByMaxPreis(BigDecimal preis) {
-//		return em.createNamedQuery(AbstractArtikel.FIND_ARTIKEL_MAX_PREIS, AbstractArtikel.class)
-//				 .setParameter(AbstractArtikel.PARAM_PREIS, preis)
-//				 .getResultList();
-//	}
-	
-	@NotNull(message = "{artikel.notFound.id}")
-	public AbstractArtikel findArtikelById(Long id) {
-		if (id == null) {
-			return null;
-		}
-		// TODO Datenbanzugriffsschicht statt Mock
-		return Mock.findArtikelById(id);
-	}
-	
-//	public List<AbstractArtikel> findAllArtikel() {
-//		// TODO Datenbanzugriffsschicht statt Mock
-//		return Mock.findAllArtikel();
-//	}
-	
-	
-	public <T extends AbstractArtikel> T createArtikel(T artikel) {
-		if (artikel == null) {
-			return artikel;
-		}
-		// TODO Datenbanzugriffsschicht statt Mock
-		artikel = Mock.createArtikel(artikel);
 
-		return artikel;
-	}
 	
-	public <T extends AbstractArtikel> T updateArtikel(T artikel) {
-		if (artikel == null) {
-			return null;
-		}
-
-		// TODO Datenbanzugriffsschicht statt Mock
-		Mock.updateArtikel(artikel);
-		
-		return artikel;
-	}
-
-	public void deleteArtikel(Long artikelId) {
-		final AbstractArtikel artikel = findArtikelById(artikelId);  // Kein Aufruf als Business-Methode
-		if (artikel == null) {
-			return;
+	/**
+	 * Suche die Artikel mit gleicher Bezeichnung
+	 * @param bezeichnung Gemeinsame Bezeichnung der gesuchten Artikel
+	 * @return Liste der gefundenen Artikel
+	 * @exception ConstraintViolationException zu @Size, falls die Liste leer ist
+	 */
+	@Size(min = 1, message = "{artikel.notFound.bezeichnung}")
+	public List<AbstractArtikel> findArtikelByBezeichnung(String bezeichnung) {
+		if (Strings.isNullOrEmpty(bezeichnung)) {
+			return findVerfuegbareArtikel();
 		}
 		
-		// TODO Datenbanzugriffsschicht statt Mock
-		Mock.deleteArtikel(artikel);
+		return em.createNamedQuery(AbstractArtikel.FIND_ARTIKEL_BY_NAME, AbstractArtikel.class)
+				 .setParameter(AbstractArtikel.PARAM_NAME, "%" + bezeichnung + "%")
+				 .getResultList();
+	}
+	
+	/**
+	 * Suche Artikel, die preiswerter als ein bestimmter Preis sind
+	 * @param preis Maximaler Preis
+	 * @return Liste der Artikel mit einem geringeren Preis als die angegebene Obergrenze
+	 * @exception ConstraintViolationException zu @Size, falls die Liste leer ist
+	 */
+	@Size(min = 1, message = "{kunde.notFound.maxPreis}")
+	public List<AbstractArtikel> findArtikelByMaxPreis(BigDecimal preis) {
+		return em.createNamedQuery(AbstractArtikel.FIND_ARTIKEL_MAX_PREIS, AbstractArtikel.class)
+				 .setParameter(AbstractArtikel.PARAM_PREIS, preis)
+				 .getResultList();
 	}
 }
+
+
+//package shop.Artikelverwaltung.service;
+//
+//import java.io.Serializable;
+//import java.lang.invoke.MethodHandles;
+////import java.math.BigDecimal;
+//import java.util.Collections;
+//import java.util.List;
+//
+//import shop.util.Mock;
+//
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
+//import javax.enterprise.context.Dependent;
+//import javax.inject.Inject;
+//import javax.persistence.EntityManager;
+//import javax.persistence.criteria.CriteriaBuilder;
+//import javax.persistence.criteria.CriteriaQuery;
+//import javax.persistence.criteria.Path;
+//import javax.persistence.criteria.Predicate;
+//import javax.persistence.criteria.Root;
+//import javax.validation.constraints.NotNull;
+//
+//import org.jboss.logging.Logger;
+//
+////import com.google.common.base.Strings;
+//
+//import shop.Artikelverwaltung.domain.AbstractArtikel;
+//import shop.util.interceptor.Log;
+//
+///**
+// * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+// */
+//
+//@Dependent
+//@Log
+//public class ArtikelService implements Serializable {
+//	private static final long serialVersionUID = 3076865030092242363L;
+//	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
+//	
+//	@Inject
+//	private transient EntityManager em;
+//	
+//	@PostConstruct
+//	private void postConstruct() {
+//		LOGGER.debugf("CDI-faehiges Bean %s wurde erzeugt", this);
+//	}
+//	
+//	@PreDestroy
+//	private void preDestroy() {
+//		LOGGER.debugf("CDI-faehiges Bean %s wird geloescht", this);
+//	}
+//	
+////	/**
+////	 * Suche nach verfuegbaren Artikeln.
+////	 * @return Liste der verfuegbaren Artikel.
+////	 */
+////	public List<AbstractArtikel> findVerfuegbareArtikel() {
+////		return em.createNamedQuery(AbstractArtikel.FIND_VERFUEGBARE_ARTIKEL, AbstractArtikel.class)
+////				 .getResultList();
+////	}
+////
+////	
+////	/**
+////	 * Suche den Artikel zu gegebener ID.
+////	 * @param id ID des gesuchten Artikels.
+////	 * @return Der gefundene Artikel, null sonst.
+////	 */
+////	public AbstractArtikel findArtikelById(Long id) {
+////		return em.find(AbstractArtikel.class, id);
+////	}
+////	
+//	/**
+//	 * Suche die Artikel zu gegebenen IDs. 
+//	 * @param ids Liste der IDs
+//	 * @return Liste der gefundenen Artikel
+//	 */
+//	public List<AbstractArtikel> findArtikelByIds(List<Long> ids) {
+//		if (ids == null || ids.isEmpty()) {
+//			return Collections.emptyList();
+//		}
+//		
+//		/*
+//		 * SELECT a
+//		 * FROM   Artikel a
+//		 * WHERE  a.id = ? OR a.id = ? OR ...
+//		 */
+//		final CriteriaBuilder builder = em.getCriteriaBuilder();
+//		final CriteriaQuery<AbstractArtikel> criteriaQuery = builder.createQuery(AbstractArtikel.class);
+//		final Root<AbstractArtikel> a = criteriaQuery.from(AbstractArtikel.class);
+//
+//		final Path<Long> idPath = a.get("id");
+//		//final Path<String> idPath = a.get(Artikel_.id);   // Metamodel-Klassen funktionieren nicht mit Eclipse
+//		
+//		Predicate pred = null;
+//		if (ids.size() == 1) {
+//			// Genau 1 id: kein OR notwendig
+//			pred = builder.equal(idPath, ids.get(0));
+//		}
+//		else {
+//			// Mind. 2x id, durch OR verknuepft
+//			final Predicate[] equals = new Predicate[ids.size()];
+//			int i = 0;
+//			for (Long id : ids) {
+//				equals[i++] = builder.equal(idPath, id);
+//			}
+//			
+//			pred = builder.or(equals);
+//		}
+//		
+//		criteriaQuery.where(pred);
+//		
+//		return em.createQuery(criteriaQuery)
+//		         .getResultList();
+//	}
+////
+////	
+////	/**
+////	 * Suche die Artikel mit gleicher Bezeichnung
+////	 * @param bezeichnung Gemeinsame Bezeichnung der gesuchten Artikel
+////	 * @return Liste der gefundenen Artikel
+////	 */
+////	public List<AbstractArtikel> findArtikelByName(String name) {
+////		if (Strings.isNullOrEmpty(name)) {
+////			return findVerfuegbareArtikel();
+////		}
+////		
+////		return em.createNamedQuery(AbstractArtikel.FIND_ARTIKEL_BY_NAME, AbstractArtikel.class)
+////				 .setParameter(AbstractArtikel.PARAM_NAME, "%" + name + "%")
+////				 .getResultList();
+////	}
+////	
+////	/**
+////	 * Suche Artikel, die preiswerter als ein bestimmter Preis sind
+////	 * @param preis Maximaler Preis
+////	 * @return Liste der Artikel mit einem geringeren Preis als die angegebene Obergrenze
+////	 */
+////	public List<AbstractArtikel> findArtikelByMaxPreis(BigDecimal preis) {
+////		return em.createNamedQuery(AbstractArtikel.FIND_ARTIKEL_MAX_PREIS, AbstractArtikel.class)
+////				 .setParameter(AbstractArtikel.PARAM_PREIS, preis)
+////				 .getResultList();
+////	}
+//	
+//	@NotNull(message = "{artikel.notFound.id}")
+//	public AbstractArtikel findArtikelById(Long id) {
+//		if (id == null) {
+//			return null;
+//		}
+//		// TODO Datenbanzugriffsschicht statt Mock
+//		return Mock.findArtikelById(id);
+//	}
+//	
+////	public List<AbstractArtikel> findAllArtikel() {
+////		// TODO Datenbanzugriffsschicht statt Mock
+////		return Mock.findAllArtikel();
+////	}
+//	
+//	
+//	public <T extends AbstractArtikel> T createArtikel(T artikel) {
+//		if (artikel == null) {
+//			return artikel;
+//		}
+//		// TODO Datenbanzugriffsschicht statt Mock
+//		artikel = Mock.createArtikel(artikel);
+//
+//		return artikel;
+//	}
+//	
+//	public <T extends AbstractArtikel> T updateArtikel(T artikel) {
+//		if (artikel == null) {
+//			return null;
+//		}
+//
+//		// TODO Datenbanzugriffsschicht statt Mock
+//		Mock.updateArtikel(artikel);
+//		
+//		return artikel;
+//	}
+//
+//	public void deleteArtikel(Long artikelId) {
+//		final AbstractArtikel artikel = findArtikelById(artikelId);  // Kein Aufruf als Business-Methode
+//		if (artikel == null) {
+//			return;
+//		}
+//		
+//		// TODO Datenbanzugriffsschicht statt Mock
+//		Mock.deleteArtikel(artikel);
+//	}
+//}
--- shop/src/main/java/shop/Artikelverwaltung/service/ArtikelServiceMock.java
+++ shop/src/main/java/shop/Artikelverwaltung/service/ArtikelServiceMock.java
@@ -1,46 +1,46 @@
-package shop.Artikelverwaltung.service;
-
-import javax.enterprise.context.Dependent;
-
-import shop.Artikelverwaltung.domain.AbstractArtikel;
-import shop.Artikelverwaltung.domain.Ersatzteil;
-import shop.Artikelverwaltung.domain.Fahrrad;
-import shop.Artikelverwaltung.domain.Sicherheitszubehoer;
-import shop.util.cdi.MockService;
-import shop.util.interceptor.Log;
-
-/**
- * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
- */
-@MockService
-@Dependent
-@Log
-public class ArtikelServiceMock extends ArtikelService {
-	private static final long serialVersionUID = -2919310633845009282L;
-	private static final int FAHVAL = 200;
-	private static final int ERSVAL = 300;
-	private static final int SICHVAL = 400;
-
-	/**
-	 * {inheritDoc}
-	 */
-	@Override
-	public AbstractArtikel findArtikelById(Long id) {
-		if (id == null) {
-			return null;
-		}
-		
-		final AbstractArtikel artikel;
-		if (FAHVAL < id && id < ERSVAL)
-			artikel = new Fahrrad();
-		else if (ERSVAL < id && id < SICHVAL)
-			artikel = new Ersatzteil();
-		else
-			artikel = new Sicherheitszubehoer();
-		
-		artikel.setId(id);
-		artikel.setName("Name: " + id);
-		
-		return artikel;	
-	}
-}
+//package shop.Artikelverwaltung.service;
+//
+//import javax.enterprise.context.Dependent;
+//
+//import shop.Artikelverwaltung.domain.AbstractArtikel;
+//import shop.Artikelverwaltung.domain.Ersatzteil;
+//import shop.Artikelverwaltung.domain.Fahrrad;
+//import shop.Artikelverwaltung.domain.Sicherheitszubehoer;
+//import shop.util.cdi.MockService;
+//import shop.util.interceptor.Log;
+//
+///**
+// * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+// */
+//@MockService
+//@Dependent
+//@Log
+//public class ArtikelServiceMock extends ArtikelService {
+//	private static final long serialVersionUID = -2919310633845009282L;
+//	private static final int FAHVAL = 200;
+//	private static final int ERSVAL = 300;
+//	private static final int SICHVAL = 400;
+//
+//	/**
+//	 * {inheritDoc}
+//	 */
+//	@Override
+//	public AbstractArtikel findArtikelById(Long id) {
+//		if (id == null) {
+//			return null;
+//		}
+//		
+//		final AbstractArtikel artikel;
+//		if (FAHVAL < id && id < ERSVAL)
+//			artikel = new Fahrrad();
+//		else if (ERSVAL < id && id < SICHVAL)
+//			artikel = new Ersatzteil();
+//		else
+//			artikel = new Sicherheitszubehoer();
+//		
+//		artikel.setId(id);
+//		artikel.setName("Name: " + id);
+//		
+//		return artikel;	
+//	}
+//}
