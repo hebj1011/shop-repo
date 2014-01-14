0b0dbd73f632dc6caa5f45b126145e93f35ed7fb
Björn Hetzel <hebj1011@hs-karlsruhe.de>
2014-01-14 18:13:06 +0100

    weiter angepasst

--- shop/src/main/resources/sql/insert.sql
+++ shop/src/main/resources/sql/insert.sql
@@ -3,10 +3,10 @@
 -- Kommentare durch -- am Zeilenanfang
 -- ===============================================================================
 
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('S',300,'Tisch ''Oval''',80,0,'01.08.2006 00:00:00','01.08.2006 00:00:00');
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('F',301,'Stuhl ''Sitz bequem''',10,0,'02.08.2006 00:00:00','02.08.2006 00:00:00');
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('E',302,'Tür ''Hoch und breit''',300,0,'03.08.2006 00:00:00','03.08.2006 00:00:00');
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('E',303,'Fenster ''Glasklar''',150,0,'04.08.2006 00:00:00','04.08.2006 00:00:00');
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('F',304,'Spiegel ''Mach mich schöner''',60,0,'05.08.2006 00:00:00','05.08.2006 00:00:00');
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('S',305,'Kleiderschrank ''Viel Platz''',500,0,'06.08.2006 00:00:00','06.08.2006 00:00:00');
-INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert) VALUES ('F',306,'Bett ''Mit Holzwurm''',600,0,'07.08.2006 00:00:00','07.08.2006 00:00:00');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('S',300,'Tisch ''Oval''',80,0,'01.08.2006 00:00:00','01.08.2006 00:00:00', 'Gross');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('F',301,'Stuhl ''Sitz bequem''',10,0,'02.08.2006 00:00:00','02.08.2006 00:00:00');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('E',302,'Tür ''Hoch und breit''',300,0,'03.08.2006 00:00:00','03.08.2006 00:00:00');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('E',303,'Fenster ''Glasklar''',150,0,'04.08.2006 00:00:00','04.08.2006 00:00:00');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('F',304,'Spiegel ''Mach mich schöner''',60,0,'05.08.2006 00:00:00','05.08.2006 00:00:00');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('S',305,'Kleiderschrank ''Viel Platz''',500,0,'06.08.2006 00:00:00','06.08.2006 00:00:00', 'Gross');
+INSERT INTO artikel (art, id, name, einzelpreis, ausgesondert, erzeugt, aktualisiert, groesse) VALUES ('F',306,'Bett ''Mit Holzwurm''',600,0,'07.08.2006 00:00:00','07.08.2006 00:00:00');
