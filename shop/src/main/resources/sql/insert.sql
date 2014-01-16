34e7862ee15e844586587f94f1a4180a136705ff
Denis Langer <lade1011(at)hs-karlsruhe.de>
2014-01-16 17:04:37 +0100

    Insert und create SQL die Spalte Geschlecht und Familienstand hinzugefügt

--- shop/src/main/resources/sql/insert.sql
+++ shop/src/main/resources/sql/insert.sql
@@ -14,12 +14,14 @@ INSERT INTO artikel (art, id, name, einzelpreis,bestand, ausgesondert, erzeugt, 
 --
 -- kunde
 --
-INSERT INTO kunde (id, nachname, vorname, seit, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (100,'Hans','Treuer','01.01.2001',1,NULL,NULL,'hans@hs-karlsruhe.de','1','01.08.2006 00:00:00','01.08.2006 00:00:00');
-INSERT INTO kunde (id, nachname, vorname, seit, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (101,'Lustig','Peter','01.07.1990',1,'0,1','300','lustig@hs-karlsruhe.de','12345678','16.01.2014 00:00:00','16.01.2014 00:00:00');
-INSERT INTO kunde (id, nachname, vorname, seit, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (102,'Brady','Tom','01.04.1990',1,'0,15','130','brady@hs-karlsruhe.de','7777','16.01.2014 00:00:00','16.01.2014 00:00:00');
-INSERT INTO kunde (id, nachname, vorname, seit, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (103,'Gronkowski','Robert','12.04.1992',1,'0,04','500','gronkwoski@hs-karlsruhe.de','171717','16.01.2014 00:00:00','16.01.2014 00:00:00');
-INSERT INTO kunde (id, nachname, vorname, seit, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (104,'Manning','Peyton','09.06.1994',1,null,'1200','manning@hs-karlsruhe.de','237','16.01.2014 01:10:00','16.01.2014 01:10:00');
-INSERT INTO kunde (id, nachname, vorname, seit, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (105,'Kaepernick','Colin','21.10.1996',1,'0,1','230','kaepernick@hs-karlsruhe.de','17','16.01.2014 01:10:00','16.01.2014 01:10:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (1,'Admin','Admin','01.01.2001',NULL,NULL,1,NULL,NULL,'admin@hs-karlsruhe.de','1','01.08.2006 00:00:00','01.08.2006 00:00:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (100,'Hans','Treuer','01.01.2001','VH','M',1,NULL,NULL,'hans@hs-karlsruhe.de','1','01.08.2006 00:00:00','01.08.2006 00:00:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (101,'Lustig','Peter','01.07.1990','VH','M',1,'0,1','300','lustig@hs-karlsruhe.de','12345678','16.01.2014 00:00:00','16.01.2014 00:00:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (102,'Brady','Tom','01.04.1990',NULL,'M',1,'0,15','130','brady@hs-karlsruhe.de','7777','16.01.2014 00:00:00','16.01.2014 00:00:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (103,'Gronkowski','Robert','12.04.1992','G','W',1,'0,04','500','gronkwoski@hs-karlsruhe.de','171717','16.01.2014 00:00:00','16.01.2014 00:00:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (104,'Manning','Peyton','09.06.1994','L','W',1,null,'1200','manning@hs-karlsruhe.de','237','16.01.2014 01:10:00','16.01.2014 01:10:00');
+INSERT INTO kunde (id, nachname, vorname, seit, familienstand, geschlecht, newsletter, rabatt, umsatz, email, password, erzeugt, aktualisiert) VALUES (105,'Kaepernick','Colin','21.10.1996','VW','M',1,'0,1','230','kaepernick@hs-karlsruhe.de','17','16.01.2014 01:10:00','16.01.2014 01:10:00');
+
 
 --
 -- adresse
